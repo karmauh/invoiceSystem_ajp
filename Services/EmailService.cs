362fd56dc7987ec15dfd7ec18fe6db0f1e7a9703
karmauh <mikolaj.bozko01@gmail.com>
2026-01-11 10:50:57 +0100

    add email auth. service

---
+++ Services/EmailService.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Configuration;
+
+namespace InvoiceSystem.Services
+{
+    public static class EmailService
+    {
+        public static void SendCode(string toEmail, string code)
+        {
+            string fromEmail = ConfigurationManager.AppSettings["SmtpEmail"];
+            string password = ConfigurationManager.AppSettings["SmtpPassword"];
+            string host = ConfigurationManager.AppSettings["SmtpHost"];
+            int port = int.Parse(ConfigurationManager.AppSettings["SmtpPort"]);
+
+            using (MailMessage message = new MailMessage())
+            {
+                message.From = new MailAddress(fromEmail, "Invoice System");
+                message.To.Add(toEmail);
+                message.Subject = "Kod weryfikacyjny logowania";
+                message.SubjectEncoding = Encoding.UTF8;
+
+                message.Body = $@"
+<html>
+<head><meta charset='UTF-8'></head>
+<body style='font-family: Arial;'>
+<h2>Logowanie do systemu</h2>
+<p>Twój jednorazowy kod weryfikacyjny:</p>
+<p style='font-size:20px; font-weight:bold;'>{code}</p>
+<p>Kod jest ważny przez 5 minut.</p>
+</body>
+</html>";
+
+                message.BodyEncoding = Encoding.UTF8;
+                message.IsBodyHtml = true;
+
+                using (SmtpClient client = new SmtpClient(host, port))
+                {
+                    client.EnableSsl = true;
+                    client.UseDefaultCredentials = false;
+                    client.Credentials = new NetworkCredential(fromEmail, password);
+                    client.Send(message);
+                }
+            }
+        }
+    }
+}
