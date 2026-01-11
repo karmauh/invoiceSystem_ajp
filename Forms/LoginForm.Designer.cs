7f2820c41e3e859274e0d2c43bb312888da5c09c
karmauh <mikolaj.bozko01@gmail.com>
2026-01-11 10:45:33 +0100

    add login form designer file

---
+++ Forms/LoginForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace InvoiceSystem.Forms
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtEmail = new TextBox();
+            txtPassword = new TextBox();
+            label2 = new Label();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(36, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(12, 27);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(139, 23);
+            txtEmail.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(12, 88);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(139, 23);
+            txtPassword.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 70);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Hasało";
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(12, 117);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(139, 36);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Zaloguj";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(170, 184);
+            Controls.Add(btnLogin);
+            Controls.Add(label2);
+            Controls.Add(txtPassword);
+            Controls.Add(txtEmail);
+            Controls.Add(label1);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtEmail;
+        private TextBox txtPassword;
+        private Label label2;
+        private Button btnLogin;
+    }
+}
