fe209f7284403a9d7d98da8cec2a59f0743567c4
karmauh <mikolaj.bozko01@gmail.com>
2026-01-11 10:50:19 +0100

    add database connection class

---
+++ Data/DatabaseConnection.cs
@@ -0,0 +1,13 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace InvoiceSystem.Data
+{
+    public static class DatabaseConnection
+    {
+        public static MySqlConnection GetConnection()
+        {
+            string connectionString = "Server=localhost;Port=3306;Database=invoicesystem;Uid=root;Pwd=rootpassword;";
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
